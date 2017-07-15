27101ef59926df311b9549aa0daddf0b483d4a08
Daigo Fujiwara <daigo@daigofujiwara.com>
2017-07-15 13:59:17 -0400

    Remove bower, ignore node_modules

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+
+var sassPaths = [
+  'node_modules/foundation-sites/scss',
+  'node_modules/foundation-sites/_vendor/normalize-scss/sass'
+];
+
+gulp.task('sass', function() {
+  return gulp.src('scss/app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['sass'], function() {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+});
